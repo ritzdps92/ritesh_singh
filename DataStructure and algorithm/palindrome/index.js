599052db9f10c281eb705db6db3a8047469aca64
rit <rit@esi-group.com>
2018-10-12 09:46:26 +0530

    Palindrome

---
+++ DataStructure and algorithm/palindrome/index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome(str) {
+
+const arr =str.split('');
+arr.reverse();
+
+const temp = arr.join('');
+if(str == temp)
+{
+  return true;
+}
+else {
+  return false;
+}
+
+
+
+
+}
+
+module.exports = palindrome;
